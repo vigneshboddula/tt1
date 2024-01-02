af29d629ffcdf52c3bf69dce8d625f7fe95ddc71
vigneshboddula <96077036+vigneshboddula@users.noreply.github.com>
2024-01-02 10:11:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, Dew Drop Service!"
 
-if _name_ == "_main_":
+if _name_ == "__main__":
     app.run(host='0.0.0.0')
     
